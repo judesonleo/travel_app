1387c6a6c632f6f72d924ac86808d8dbbd56ae18
Judeson Leo <judesonleo123@gmail.com>
2024-04-24 14:40:22 +0530

    Guide Section Done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Travel App",
@@ -17,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Navbar /> 
         <main className="relative overflow-hidden ">
         {children}
